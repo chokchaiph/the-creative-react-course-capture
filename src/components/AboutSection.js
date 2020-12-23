08e53440b8228d9919fb9f812f1b37f794aff585
chokchaiph <chokchai.ph@gmail.com>
2020-12-23 15:20:01 +0700

    6. Styling Services Section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,23 +2,29 @@ import React from "react";
 import home1 from "../img/home1.png";
 //styled
 import styled from "styled-components";
+import {
+  StyledAbout,
+  StyledDescription,
+  StyledHide,
+  StyledImage,
+} from "../styles";
 
 const AboutSection = () => {
   return (
     <StyledAbout>
       <StyledDescription>
         <div className="title">
-          <SytledHide>
+          <StyledHide>
             <h2>We work to make</h2>
-          </SytledHide>
-          <SytledHide>
+          </StyledHide>
+          <StyledHide>
             <h2>
               your <span>dreams</span> come
             </h2>
-          </SytledHide>
-          <SytledHide>
+          </StyledHide>
+          <StyledHide>
             <h2>true.</h2>
-          </SytledHide>
+          </StyledHide>
         </div>
         <p>
           Contact us for any photography or videography ideas that you have. We
@@ -34,35 +40,5 @@ const AboutSection = () => {
 };
 
 //styled
-const StyledAbout = styled.div`
-  min-height: 90vh;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 5rem 10rem;
-  color: white;
-`;
-
-const StyledDescription = styled.div`
-  flex: 1;
-  padding-right: 5rem;
-  h2 {
-    font-weight: lighter;
-  }
-`;
-
-const StyledImage = styled.div`
-  flex: 1;
-  overflow: hidden;
-  img {
-    width: 100%;
-    height: 80vh;
-    object-fit: cover;
-  }
-`;
-
-const SytledHide = styled.div`
-  overflow: hidden;
-`;
 
 export default AboutSection;
