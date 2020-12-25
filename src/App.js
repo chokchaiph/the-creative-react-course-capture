f2e95b87e64013af98e3aa9c39390d6e87b2697d
chokchaiph <chokchai.ph@gmail.com>
2020-12-25 15:53:56 +0700

    9. React Router

--- src/App.js
+++ src/App.js
@@ -3,13 +3,27 @@ import AboutUs from "./pages/AboutUs";
 //import GlobalStyle
 import GlobalStyle from "./components/GlobalStyle";
 import Nav from "./components/Nav";
+import OurWork from "./components/OurWork";
+import ContactUs from "./components/ContactUs";
+
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,7 +1,6 @@
 import React from "react";
 import home1 from "../img/home1.png";
 //styled
-import styled from "styled-components";
 import {
   StyledAbout,
   StyledDescription,
