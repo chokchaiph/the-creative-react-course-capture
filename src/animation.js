b3915ca5a4c82da0320212c1d7868bcf277ecd4b
chokchaiph <chokchai.ph@gmail.com>
2020-12-30 13:39:02 +0700

    16. Our Work Animation

--- src/animation.js
+++ src/animation.js
@@ -14,7 +14,7 @@ export const pageAnimation = {
   },
   exit: {
     opacity: 0,
-    y: 300,
+    // y: 300,
     transition: {
       duration: 0.5,
     },
@@ -58,6 +58,42 @@ export const photoAnim = {
     transition: {
       duration: 0.75,
       ease: "easeOut",
+    },
+  },
+};
+
+export const lineAnim = {
+  hidden: {
+    width: "0%",
+  },
+  show: {
+    width: "100%",
+    transition: {
+      duration: 1,
+    },
+  },
+};
+
+export const slider = {
+  hidden: { x: "-130%", skew: "45deg" },
+  show: {
+    x: "100%",
+    skew: "0deg",
+    transition: {
+      ease: "easeOut",
+      duration: 1,
+    },
+  },
+};
+
+export const sliderContainer = {
+  hidden: {
+    opacity: 1,
+  },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.1,
     },
   },
 };
